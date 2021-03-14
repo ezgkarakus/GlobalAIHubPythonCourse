86960badb06822323549b0d0493fb32a3bd758dd
Özge Dinçer <67578643+ozgedincer@users.noreply.github.com>
2021-03-14 20:25:24 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,6 @@
-#Explain your work
-
-#Question 1
-for x in range(a):
-	print(a)
+list1=[i for i in range(10) if i%2==0]
+list2=[i for i in range(10) if i%2==1]
+list1.extend(list2)
+list1=[a*2 for a in list1]
+for i in range(len(list1)):
+    print(type(list1[i]))
