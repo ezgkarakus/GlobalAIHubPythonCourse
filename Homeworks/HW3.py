a753b5bf808d142853a735a7bb74a3547d43e0fd
Özge Dinçer <67578643+ozgedincer@users.noreply.github.com>
2021-03-14 20:28:17 +0300

    Update HW3.py

--- Homeworks/HW3.py
+++ Homeworks/HW3.py
@@ -1,5 +1,15 @@
-#Explain your work
+def cal_passing_grade(midt, project, final):
+    passing_grade = midt * 0.3 + project * 0.3 + final * 0.4
+    return passing_grade
 
-#Question 1
-for x in range(a):
-	print(a)
+st_list = [None] * 5
+for index in range(5):
+    print("Write student's info (name-surname-midterm grade-project grade-final grade)")
+    st = {"Name": input(), "Surname": input(), "Midterm": float(input()), "Project": float(input()), "Final Project": float(input())}
+    st["Passing Grade"] = cal_passing_grade(st["Midterm"], st["Project"], st["Final Project"])
+    st_list[index] = st
+
+st_list.sort(key=lambda j: j["Passing Grade"], reverse=True)
+
+for st in st_list:
+    print(st)
