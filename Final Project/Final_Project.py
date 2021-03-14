59a6607653f5ee23066b3c8f8c3173e9eedf87ee
Özge Dinçer <67578643+ozgedincer@users.noreply.github.com>
2021-03-14 20:28:55 +0300

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,22 @@
-import pandas as pd
+questions = {"1: What nationality is David Beckham?" : "english",
+             "2: What is the Capital of Turkey?": "ankara",
+             "3: Where is the Anıtkabir?:" : "ankara",
+             "4: Who wrote The Metamorphosis ?": "franz kafka",
+             "5: When is Halloween? ": "31st october",
+             "6:What is the eighth month of the year?": "august",
+             "7: What is the currency in Japan?": "japanese yen",
+             "8: Which year did Mustafa Kemal Atatürk died?": "1938",
+             "9: Where is the head office of the Global IA Hub": "switzerland",
+             "10: -What does WHO stand for?": "world health organisation"
+             }
+score = 0
+for question, answer in questions.items():
+    print(f"QUESTION {question}")
+    user_answer = input().lower()
+    if user_answer == answer:
+        score += 10
 
-data = pd.read_csv("project.csv")
+if score <= 50:
+    print(f"!YOU LOST!{score}")
+elif score > 50:
+    print(f"!YOU WON! SCORE:{score}")
